9de2683e6f14a432e837bf9fb8de7df813751830
rohitgeddam <rohitgeddam2018@gmail.com>
2022-11-28 22:49:36 -0500

    try catch weather api error

--- info/views.py
+++ info/views.py
@@ -19,15 +19,20 @@ def place_photo(request):
 def info_page(request):
     city = request.GET.get("city")
     country = request.GET.get("country")
+    
+    try:
 
-    weather_info = WeatherBitHelper().get_city_weather(city=city, country=country)["data"][0]
+        weather_info = WeatherBitHelper().get_city_weather(city=city, country=country)["data"][0]
 
-    weather_info["sunrise"] = datetime.strptime(weather_info["sunrise"], "%H:%M").astimezone(
-        pytz.timezone(weather_info['timezone'])).strftime("%I:%M")
-    weather_info["sunset"] = datetime.strptime(weather_info["sunset"], "%H:%M").astimezone(
-        pytz.timezone(weather_info['timezone'])).strftime("%I:%M")
-    weather_info["ts"] = datetime.fromtimestamp(weather_info["ts"]).strftime("%m-%d-%Y, %H:%M")
+        weather_info["sunrise"] = datetime.strptime(weather_info["sunrise"], "%H:%M").astimezone(
+            pytz.timezone(weather_info['timezone'])).strftime("%I:%M")
+        weather_info["sunset"] = datetime.strptime(weather_info["sunset"], "%H:%M").astimezone(
+            pytz.timezone(weather_info['timezone'])).strftime("%I:%M")
+        weather_info["ts"] = datetime.fromtimestamp(weather_info["ts"]).strftime("%m-%d-%Y, %H:%M")
 
+    except:
+        weather_info = {}
+    
     dining_info = FourSquarePlacesHelper().get_places(
         city=f"{city}, {country}", categories="13065", sort="RELEVANCE", limit=5)
     airport_info = FourSquarePlacesHelper().get_places(
@@ -39,6 +44,7 @@ def info_page(request):
 
     photo_link = UnplashCityPhotoHelper().get_city_photo(city=city)
 
+    print(dining_info)
     return render(
         request, 'search/city_info.html',
         context={
